349d5335aa9a79f9241555ce0b82aa1f9fc5189a
Mirabelljoicelaura <mirabelljoice@gmail.com>
2023-02-25 17:26:10 +0700

    Praktikum 2 - Controller Web Laravel Single

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MiraController;
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
@@ -43,6 +46,11 @@ use Illuminate\Support\Facades\Route;
 // });
 
 //JOBSHEET 2 Praktikum 2
-Route::get('/',[PageController::class,"index"]);
-Route::get('/about',[PageController::class,"about"]);
-Route::get('/articles/{id}',[PageController::class,"articles"]);
+Route::get('/',[HomeController::class,"index"]);
+Route::get('/about',[AboutController::class,"about"]);
+Route::get('/articles/{id}',[ArticleController::class,"articles"]);
+
+//JOBSHEET 2 Praktikum 3
+// Route::prefix('category')->group(function(){
+//     Route::get('/',)
+// })
