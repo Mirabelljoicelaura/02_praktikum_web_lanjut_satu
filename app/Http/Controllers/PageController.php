8c6201fda96dd9a55304e1423532314bb3618ad5
Mirabelljoicelaura <mirabelljoice@gmail.com>
2023-02-26 14:56:16 +0700

    Praktikum 3 - Desain Routing Web

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,14 +6,7 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    public function index() {
-        return 'Selamat Datang';
-    }
-    public function about() {
-        echo "Nama : Mirabell Joice Laura <br>";
-        echo "NIM : 2141720174";
-    }
-    public function articles($id) {
-        echo"Halaman artikel dengan ID : $id";
-    }
+
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\AboutusController;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ContactusController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MiraController;
+use App\Http\Controllers\NewsController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\ProgramController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -51,6 +57,20 @@ Route::get('/about',[AboutController::class,"about"]);
 Route::get('/articles/{id}',[ArticleController::class,"articles"]);
 
 //JOBSHEET 2 Praktikum 3
-// Route::prefix('category')->group(function(){
-//     Route::get('/',)
-// })
+Route::prefix('category')->group(function(){
+    Route::get('/',[CategoryController::class,"index"]);
+    Route::get('/{id}',[CategoryController::class,"show"]);
+});
+Route::prefix('news')->group(function(){
+    Route::get('/',[NewsController::class,"index"]);
+    Route::get('/{judul}',[NewsController::class,"show"]);
+});
+Route::prefix('program')->group(function(){
+    Route::get('/',[ProgramController::class,"index"]);
+    Route::get('/{id}',[ProgramController::class,"show"]);
+});
+Route::get('/aboutus',[AboutusController::class,"index"]);
+Route::resource('/contactus', ContactusController::class)->only([
+    'index', 'show'
+   ]);
+
